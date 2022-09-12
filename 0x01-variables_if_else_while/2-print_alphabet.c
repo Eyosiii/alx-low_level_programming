e6fff1d8465c12f93759e1fa00d0334abfbd9aed
eyosiii <eyositesfish@yahoo.com>
2022-09-12 15:24:03 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: shows alphabet
+ *
+ * Return: 0
+ **/
+
+int main(void)
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	return (0);
+}
