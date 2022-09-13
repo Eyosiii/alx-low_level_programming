9c03eae97c238255c9eb509a946c758a7ed5fdbb
eyosiii <eyositesfish@yahoo.com>
2022-09-13 16:35:19 -0700

    10 alpha

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,11 +8,14 @@
 
 void print_alphabet_x10(void)
 {
-	char c;
+	char c, i;
 
+	for (i = 0; i <= 9; i++)
+	{
 	for (c = 'a'; c <= 'z'; c++)
 	{
-		_putchar(c * 10);
+		_putchar(c);
 	}
-_putchar('\n');
+		_putchar('\n');
+	}
 }
