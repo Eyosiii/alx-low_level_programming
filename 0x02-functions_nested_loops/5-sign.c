428fffa99c0048c1465ec74f86c7f30930b7d21e
eyosiii <eyositesfish@yahoo.com>
2022-09-13 17:42:27 -0700

    Show positive and negative

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign - it shows wheather itspositive or negative
+ * @n: character used for numbers
+ *
+ * Return: always 0
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+		_putchar ('+');
+	}
+	else if (n == 0)
+	{
+		return (0);
+		_putchar ('0');
+	}
+	else (n < 0);
+		return (-1);
+		_putchar ('-');
+}
