ae3f33ac056452db58d95d65f8ae81e503f67fd7
eyosiii <eyositesfish@yahoo.com>
2022-09-15 06:14:41 -0700

    Upper case

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - shows if the letters are upper or not
+ *@c: character used to check
+ *
+ * Return: 0
+ */
+
+int _isupper(int c)
+{
+	char c;
+
+	if (c >= 'A'; c <= 'Z')
+		_putchar("1");
+	else
+		_putchar ("0");
+	return (0);
+}
