1790d62c8562cf48bf3d0c1791702323cd00bce4
eyosiii <eyositesfish@yahoo.com>
2022-09-16 14:53:14 -0700

    numbers and symbols

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints numbers with symbol
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 48; x <= 57; x++)
+	putchar(x);
+	putchar(44);
+	putchar(32);
+	return (0);
+}
