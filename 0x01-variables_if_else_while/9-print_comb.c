3b759f6b25487704f8144a25d727b7648b139ec7
eyosiii <eyositesfish@yahoo.com>
2022-09-16 15:08:08 -0700

    Numbers plus symbol

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,8 +13,13 @@ int main(void)
 	int x;
 
 	for (x = 48; x <= 57; x++)
-	putchar(x);
-	putchar(44);
-	putchar(32);
+	{
+		putchar(x);
+		if (x != 57)
+		{
+		putchar(',');
+		putchar(32);
+		}
+	}
 	return (0);
 }
