c77abd222f14f1e755499bb28b25554264345588
eyosiii <eyositesfish@yahoo.com>
2022-09-20 03:58:32 -0700

    Counts string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *_strlen - Counts the number of strings
+ *@s: string
+ * Return: 0
+ */
+
+int _strlen(char *s)
+{
+	int x;
+
+	for (x = 0; x != '\0'; x++)
+	return (x);
+}
