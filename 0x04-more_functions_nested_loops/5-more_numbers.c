42418a88960cf6b673fbe9c6fe2a43ab62d6ffe9
eyosiii <eyositesfish@yahoo.com>
2022-09-23 17:30:07 -0700

    Print 10 times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * more_numbers - print numbers 10 times
+ *
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int n, i;
+
+	for (n = '0'; n <= 14; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+	for (i = 1; i = 10; i++)
+		_putchar(n);
+}
