b2df4cc47fc2768d33560a656de248ebf8e17264
eyosiii <eyositesfish@yahoo.com>
2022-10-04 04:20:20 -0700

    Show string

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * @argc: the count used
+ * @argv: strings used for compilation
+ *
+ * Return: argv
+ */
+
+int main (int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,5 @@
+This is a readme on argc and argv
+
+argc is the count of arguments passed when compiling
+
+argv is the strings of arguments used while compiling
