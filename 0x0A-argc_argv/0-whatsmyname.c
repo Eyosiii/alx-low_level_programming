36c6b89f3f005e1a895c2794ef678265eae59c7f
eyosiii <eyositesfish@yahoo.com>
2022-10-04 05:03:53 -0700

    strings used to compile

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,15 +2,15 @@
 #include <stdlib.h>
 
 /**
- * @argc: the count used
- * @argv: strings used for compilation
+ * main - shows which strings used to compile
+ *@argc: string counter
+ *@argv: strings used for compilation
  *
  * Return: argv
  */
 
-int main (int argc, char *argv[])
+int main(int argc__attribute__((unused)), char *argv[])
 {
-	(void)argc;
 	printf("%s\n", argv[0]);
 	return (0);
 }
