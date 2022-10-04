4f25997ee052bbb0fe606d96baef4c3d14ca0b4f
eyosiii <eyositesfish@yahoo.com>
2022-10-04 05:22:16 -0700

    Count strings used

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - it shows how many arguments used
+ *
+ *@argc: argument counter
+ *@argv: argument shower
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d/n", argc);
+	return (0);
+}
