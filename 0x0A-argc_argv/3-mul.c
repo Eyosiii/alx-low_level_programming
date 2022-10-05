ae4ef82d077a9d334ed6f715b3bcddcad2ae9137
eyosiii <eyositesfish@yahoo.com>
2022-10-05 13:58:38 -0700

    Multiply args

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies the numbers after argument
+ *
+ *@argc: argument counter
+ *@argv: argument vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int res, n1, n2;
+
+	if (argc == 3)
+	{
+		n1 = strtol(argv[1], NULL, 10);
+		n2 = strtol(argv[2], NULL, 10);
+		res = n2 * n1;
+		printf("%d\n", res);
+	}
+	if (argc != 3)
+	{
+		printf("Error");
+	}
+	return (1);
+}
