4dbce8d1bd44eed06d1b310de7838803d1bad0dc
eyosiii <eyositesfish@yahoo.com>
2022-10-09 14:24:08 -0700

    char array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * *create_array - prints the array
+ *
+ *@c: char used
+ *@size: array size
+ * Return: pointer
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	if (size > 0)
+	{
+		for (i = 0; i < size; c++)
+		_putchar (c);
+	}
+	return (create_array);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,3 @@
+This is a readme file for malloc and free
+Malloc function allocates memory dynamically.
+Free function frees the allocated memory.
