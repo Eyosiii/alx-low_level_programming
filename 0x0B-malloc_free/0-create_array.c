29e4fb27c09897562581bddefcfce13217e6f3f3
eyosiii <eyositesfish@yahoo.com>
2022-10-10 02:33:32 -0700

    cretae array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,12 +14,16 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *x;
 
-	x = create_array(size, c);
 	if (size == 0)
 	{
 		return (NULL);
 	}
-	if (size > 0)
+	x = malloc(size * sizeof(char));
+	if (x == 0)
+	{
+		return (NULL);
+	}
+	if (x > 0)
 	{
 		for (i = 0; i < size; c++)
 		_putchar (c);
