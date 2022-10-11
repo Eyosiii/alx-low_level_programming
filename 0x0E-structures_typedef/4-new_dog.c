8df71b552096d86ef08718997038e2996605ad4b
eyosiii <eyositesfish@yahoo.com>
2022-10-11 14:19:53 -0700

    free memory

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,24 @@
+#include <stdio.h>                                                                 
+#include "dog.h"                                                                   
+#include <stdlib.h>                                                                
+                                                                               
+/**                                                                                
+ *
+ * print_dog - prints the structure                                                 *                                                                                 
+ *@name: dogs name                                                                 
+ *@age: dogs age                                                                   
+ *@owner: dogs owner                                                               
+ *                                                                                 
+ * Return: 0                                                                       
+ */                                                                                
+                                                                                 
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	if (new_dog == 0)
+		return;
+	else
+	{
+		new_dog->name = name;
+		new_dog->owner = owner;
+	}
+}
---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - frees the memory allocated
+ *
+ * @dog_t: variable used
+ * @d: another variable
+ *
+ * Return: 0
+ */
+
+void free_dog(dog_t *d)
+{
+	int *free_dog = (int*)malloc(sizeof(struct dog));
+	free(free_dog);
+}
