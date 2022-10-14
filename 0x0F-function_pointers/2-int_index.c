92e6b26dbfabf8396c8369b4782bb47fc37153d9
eyosiii <eyositesfish@yahoo.com>
2022-10-13 23:50:29 -0700

    search integer

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,33 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - function which returns index of first element
+ *
+ * @array: array used
+ * @size: no of elements
+ * @cmp: comparing function
+ *
+ * Return: index
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+	int x;
+
+	if (size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		x = cmp(array[i]);
+		if (x != 0)
+		{
+			return (i);
+		}
+		if (x == 0)
+		{
+		return (-1);
+		}
+	}
+	return (0);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,2 @@
+Its a readme file for function pointers.
+function pointers points to the function
