98f35c2da83638500db581c617436c99c05326ec
eyosiii <eyositesfish@yahoo.com>
2022-10-22 10:24:15 -0700

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/* main - Entry point
+ *
+ * Description: Shows the absolute value of integers
+ *
+ * Return: 0
+ */
+
+int _abs(int i)
+{	
+	if ((i = (-i)))
+	_putchar (i);
+	else
+	_putchar (i);	
+	return (0);
+}
